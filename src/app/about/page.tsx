53c4e84bee7cd0ecf3978ee93dfcf430a9e745de
panatte <panat.te@bumail.net>
2024-05-28 23:05:57 +0700

    upd

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,19 +1,65 @@
-import React from "react";
+"use client";
 import Link from 'next/link'
-import Navbar from "../../components/Navbar-login";
+import Navbar from "../../components/Navbar";
+import Navbar_login from "../../components/Navbar-login";
+import Navbar_admin from "../../components/Navbar-admin";
 import Footer from "../../components/Footer";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [nav, setNav] = useState("");
+  const [showPopUp, setShowPopUp] = useState("");
+  const [profile, setProfile] = useState({} as any);
+  const handleClose = () => {
+    setShowPopUp("");
+  };
+
+  const address = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port;
+  fetch(address + "/api/Checkcookies", {
+    method: "POST",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message == "Success") {
+        setNav(data.data.role);
+        setProfile(data.data.path_profile);
+      } else {
+        setNav("guest");
+      }
+    });
   return (
-    < main >
-      <Navbar />
-      <main className="flex min-h-screen flex-col items-center  p-14 bg-gray-200 text-center">
-        <div className="p-6">
-        <strong><p style={{ fontSize: '26px' }}>เกี่ยวกับ</p></strong>
-        </div>
-        <div>
-        GIFMaker ถูกสร้างเมื่อ Q4 2023 เป็นเว็บไซต์สำหรับสร้าง อัพโหลด GIF และ แชร์ผลงาน GIF Animation 
-        </div>
+    <main>
+      {String(nav) === "admin" ? (
+        <Navbar_admin />
+      ) : String(nav) === "user" ? (
+        <Navbar_login />
+      ) : (
+        <Navbar />
+      )}
+      <main className="flex min-h-screen flex-col items-center justify-between p-14 bg-gray-900">
+      <div className="grid bg-gray-800 m-[20px] p-14 min-h-[80vh] rounded-md min-w-screen">
+          <div className="text-center text-white">
+          <p className="text-3xl">About Us</p>
+          <br></br>
+          &nbsp;GIFMaker, launched in the second quarter of 2024, is a website dedicated to crafting and sharing GIF animations. To explore user-generated
+          examples, simply visit the homepage.
+          </div>
+          <hr></hr>
+          <div className="text-center text-white">
+          <p className="text-3xl">Features</p>
+          <br></br>
+          <ul>
+            <li>Create and upload GIF</li>
+            <li>Download and copy link of users' published from the main page</li>
+          </ul>
+          </div>
+          <hr></hr>
+          <div className="text-center text-white">
+          <p className="text-3xl">License</p>
+          <br></br>
+          &nbsp;At GIFMaker, we believe in the power of sharing and collaboration. We want to make our content as accessible as possible while still respecting the rights of content creators. That's why we've chosen to license our content under the <b><a href="https://creativecommons.org/licenses/by/4.0/" className="hover:underline" target="_blank" rel="noopener noreferrer">Creative Commons Attribution 4.0 International License (CC BY 4.0)</a></b>.
+          </div>
+      </div>
       </main>
       <Footer />
     </main>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,18 +5,19 @@ export default function Footer() {
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <a href="https://flowbite.com/" className="flex items-center">
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white">GIFMaker</span>
-                        </a>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white">GIFMaker</span>
                     </div>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">GIFMaker</a>. All Rights Reserved.
+                    <span className="text-sm text-white sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">GIFMaker</a>. All Rights Reserved.
+                    </span>
+                    <span className="text-sm text-white sm:text-center dark:text-gray-400">
+                        Except where otherwise noted, content on this site is licensed under a&nbsp;
+                        <b><a href="https://creativecommons.org/licenses/by/4.0/" className="hover:underline" target="_blank" rel="noopener noreferrer">Creative Commons Attribution 4.0 International License</a>.</b>
                     </span>
                 </div>
             </div>
         </footer>
-
     )
-}   
+}
