f4afbfac83861c8f3adaa5548ea565765eb0983c
panatte <panat.te@bumail.net>
2024-05-29 02:47:22 +0700

    upd

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -602,7 +602,7 @@ export class Store {
     this.addEditorElement({
       id,
       name: `Media(sticker) ${index + 1}`,
-      type: "video",
+      type: "image",
       placement: {
         x: placementX,
         y: placementY,
